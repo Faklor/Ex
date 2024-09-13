98ff80891abe51e5d1de9d9cc1d5ecb06a47c7f5
Faklor <71695325+Faklor@users.noreply.github.com>
2024-09-13 16:33:52 +0300

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"66e43c762fc7070018bbc03d",
+				"66e43c762fc7070018bbc040"
+			]
+		},
+		"66e43c762fc7070018bbc03d": {
+			"id": "66e43c762fc7070018bbc03d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"66e43c762fc7070018bbc040": {
+			"id": "66e43c762fc7070018bbc040",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
---
+++ src/pages/index.js
@@ -0,0 +1,263 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Image, Text, LinkBox, Box, Section } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Override, SocialMedia } from "@quarkly/components";
+import * as Components from "components";
+import { FiMenu } from "react-icons/fi";
+import { AiOutlineMenu } from "react-icons/ai";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section sm-padding="8px 0 8px 0" quarkly-title="Header-4">
+			<Override slot="SectionContent" flex-direction="row" justify-content="space-between" />
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="flex-start"
+				flex-direction="row"
+				width="30%"
+				sm-width="50%"
+				sm-align-items="center"
+				sm-flex-direction="row"
+				sm-justify-content="flex-start"
+				md-width="50%"
+				lg-width="70%"
+				md-justify-content="flex-start"
+			>
+				<LinkBox flex-direction="row" href="/index" display="flex" grid-gap="12px">
+					<Image src="https://uploads.quarkly.io/6457b971d28fe100213a0f35/images/flower-logo-template.svg?v=2023-08-06T19:34:51.878Z" display="block" width="36px" height="36px" />
+					<Text
+						margin="0"
+						md-margin="0px 0 0px 0"
+						text-align="left"
+						font="--headline3"
+						sm-margin="0px 0 0px 0"
+						display="block"
+					>
+						Company
+					</Text>
+				</LinkBox>
+			</Box>
+			<Components.QuarklycommunityKitMobileSidePanel
+				menuPosition="full"
+				breakpoint="lg"
+				width="70%"
+				sm-width="50%"
+				md-width="50%"
+				lg-width="30%"
+			>
+				<Override slot="Children" md-display="flex" />
+				<Override
+					slot="Content"
+					padding="0px 0px 0px 0px"
+					background="rgba(255, 255, 255, 0)"
+					lg-background="#ffffff"
+					lg-margin="0px 0px 0px 0px"
+				/>
+				<Override
+					slot="Button Text"
+					font="normal 600 16px/1.5 -apple-system, system-ui, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
+					text-transform="uppercase"
+					letter-spacing="1px"
+					sm-font="normal 600 14px/1.5 -apple-system, system-ui, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
+					sm-margin="0px 2px 0px 0px"
+					lg-margin="0px 0px 0px 0px"
+				/>
+				<Override
+					slot="Button Icon :closed"
+					category="fi"
+					icon={FiMenu}
+					size="32px"
+					padding="5px 7px 5px 7px"
+					border-radius="50px"
+				/>
+				<Override
+					slot="Button Icon"
+					width="28px"
+					height="28px"
+					category="ai"
+					icon={AiOutlineMenu}
+					color="--dark"
+					size="24px"
+					lg-width="32px"
+					lg-height="32px"
+				/>
+				<Override
+					slot="Cross"
+					lg-width="32px"
+					lg-height="32px"
+					size="32px"
+					top="24px"
+					right="24px"
+				/>
+				<Box
+					align-items="center"
+					lg-justify-content="center"
+					lg-align-items="flex-start"
+					justify-content="flex-end"
+					display="flex"
+					lg-flex-direction="column"
+					lg-margin="0px auto 0px auto"
+					lg-min-width="300px"
+					lg-max-width="1280px"
+					lg-width="90%"
+					lg-padding="24px 0px 48px 0px"
+				>
+					<Box
+						display="none"
+						lg-width="100%"
+						lg-margin="0px 0px 24px 0px"
+						lg-display="flex"
+						lg-padding="12px 0px 12px 0px"
+					>
+						<LinkBox flex-direction="row" href="/index" display="flex" grid-gap="12px">
+							<Image src="https://uploads.quarkly.io/6457b971d28fe100213a0f35/images/flower-logo-template.svg?v=2023-08-06T19:34:51.878Z" display="block" width="36px" height="36px" />
+							<Text
+								margin="0"
+								md-margin="0px 0 0px 0"
+								text-align="left"
+								font="--lead"
+								sm-margin="0px 0 0px 0"
+								display="block"
+								lg-font="--headline3"
+							>
+								Company
+							</Text>
+						</LinkBox>
+					</Box>
+					<Components.QuarklycommunityKitMenu
+						display="flex"
+						filterMode="exclude"
+						filterPages="/index"
+						grid-gap="8px"
+						lg-flex-direction="column"
+						lg-padding="6px 0px 6px 0px"
+						lg-margin="0px 0px 24px 0px"
+						align-items="center"
+						flex-wrap="wrap"
+						overflow-x="visible"
+						overflow-y="visible"
+						lg-align-items="flex-start"
+					>
+						<Override
+							slot="link"
+							color="--darkL2"
+							hover-color="--primary"
+							font="--lead"
+							text-decoration-line="initial"
+							transition="color 0.1s ease 0s"
+							lg-font="--lead"
+						/>
+						<Override
+							slot="item-active"
+							border-width="0 0 2px 0"
+							border-style="solid"
+							border-color="--color-darkL2"
+							lg-border-width="0 0 0 2px"
+						/>
+						<Override slot="item" padding="6px 8px 6px 8px" lg-padding="6px 12px 6px 12px" />
+						<Override slot="link-active" cursor="default" color="--darkL2" hover-color="--darkL2" />
+						<Override slot="link-about" />
+					</Components.QuarklycommunityKitMenu>
+					<Box
+						width="25%"
+						display="none"
+						lg-width="100%"
+						lg-flex-direction="column"
+						lg-align-items="flex-start"
+						lg-display="flex"
+						justify-content="space-around"
+						align-items="center"
+						flex-wrap="wrap"
+						lg-margin="0px 0px 0px 0px"
+					>
+						<SocialMedia
+							instagram="https://instagram.com/instagram"
+							margin="0px 0px 0px 0px"
+							facebook="https://www.facebook.com/quarklyapp/"
+							youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+							lg-display="flex"
+							lg-grid-gap="12px"
+						>
+							<Override slot="link-twitter" margin="0px 0px 0px 5px">
+								<div />
+							</Override>
+							<Override
+								slot="link"
+								border-radius="50%"
+								background="transparent"
+								hover-color="--light"
+								display="flex"
+								margin="0 5px 0 5px"
+								padding="5x 5px 5px 5px"
+								width="32px"
+								height="32px"
+								align-items="center"
+								justify-content="center"
+							/>
+							<Override slot="icon" size="32px" border-radius="50px" color="--grey" />
+							<Override slot="link-facebook" margin="0px 5px 0px 0px">
+								<div />
+							</Override>
+						</SocialMedia>
+					</Box>
+				</Box>
+				<Link href="#" color="#000000">
+					Комбайны
+				</Link>
+				<Link href="#" color="#000000">
+					Опрыскиватели
+				</Link>
+				<Link href="#" color="#000000">
+					Погрущики
+				</Link>
+				<Link href="#" color="#000000">
+					Трактора
+				</Link>
+				<Link href="#" color="#000000">
+					Легковые автомобили
+				</Link>
+				<Link href="#" color="#000000">
+					Грузовики
+				</Link>
+			</Components.QuarklycommunityKitMobileSidePanel>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"66e43c762fc7070018bbc036"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
